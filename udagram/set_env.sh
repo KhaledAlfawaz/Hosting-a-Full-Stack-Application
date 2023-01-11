7553a1016b783294583756e6bdc37c9aa51d8871
KhaledAlfawaz <khaled@al-fawaz.net>
2023-01-11 21:10:54 +0300

    update configuration

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -7,5 +7,7 @@ export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::khaledawsbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
+export AWS_ACCESS_KEY_ID=AKIA4U6ZUSW3Q5RR75XN
+export AWS_SECRET_ACCESS_KEY=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
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
